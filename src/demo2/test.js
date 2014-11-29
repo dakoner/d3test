aab1cabb6e4c096c34f2f51ab57eb4aba3825ee6
David Konerding <dakoner@gmail.com>
2014-11-29 11:51:06 -0800

    working multi-line scrollable view.

--- src/demo2/test.js
+++ src/demo2/test.js
@@ -2,8 +2,8 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var parseDate = d3.time.format("%Y-%m-%d").parse,
-    formatDate = d3.time.format("%Y");
+var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse,
+    formatDate = d3.time.format("%Y-%m-%d");
 
 var x = d3.time.scale()
     .range([0, width]);
@@ -23,16 +23,13 @@ var yAxis = d3.svg.axis()
 
 var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.value); });
+    .y(function(d) { return y(d.outside_temp); });
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var zoom = d3.behavior.zoom()
-    .on("zoom", draw);
 
 
 
@@ -45,48 +42,76 @@ svg.append("g")
     .attr("class", "y axis")
     .attr("transform", "translate(" + width + ",0)");
 
-svg.append("clipPath")
-    .attr("id", "clip")
-  .append("rect")
-    .attr("x", x(0))
-    .attr("y", y(1))
-    .attr("width", x(1) - x(0))
-    .attr("height", y(0) - y(1));
 
-svg.append("path")
-    .attr("class", "line")
-    .attr("clip-path", "url(#clip)");
+d3.csv("outside_temp.csv", function(error, data) {
+  data.forEach(function(d) {
+    d.date = parseDate(d.created_at);
+    d.outside_temp = parseFloat(d.outside_temp);
+  });
 
-svg.append("rect")
+  x.domain([new Date(2014, 10, 27), new Date(2014, 11, 0)]);
+  y.domain(d3.extent(data, function(d) { return d.outside_temp; }));
+
+
+   var dataNest = d3.nest()
+        .key(function(d) { return d["station.uuid"]; })
+        .entries(data);
+
+  var color = d3.scale.category10();
+  legendSpace = width/dataNest.length; // spacing for legend
+
+  dataNest.forEach(function(d, i) {
+                svg.append("path")
+                    .attr("class", "line")
+                    .style("stroke", function() {
+                                    return d.color = color(d.key); })
+                    .attr("d", line(d.values));
+
+                svg.append("text")
+                    .attr("x", (legendSpace/2)+i*legendSpace) // spacing
+                    .attr("y", height + (margin.bottom/2)+ 25)
+                    .attr("class", "legend")
+                    .style("fill", function() {
+                        return d.color = color(d.key); })
+                    .text(d.key);
+                    }
+    );
+
+    var draw = function() {
+      svg.select("g.x.axis").call(xAxis).selectAll("text")
+                                          .style("text-anchor", "end")
+                                          .attr("dx", "-.8em")
+                                          .attr("dy", ".15em")
+                                          .attr("transform", function(d) {
+                                            return "rotate(-90)";
+                                          });
+      svg.select("g.y.axis").call(yAxis);
+
+      svg.selectAll("path.line")[0].forEach(function(d, i) {
+        d.attributes['d'].value=line(dataNest[i].values);
+      });
+/*
+      svg.select("path.line").attr("d", function(d) {
+        s = [];
+        dataNest.forEach(function(x, i) {
+            s.push(line(x.values));
+        });
+        return s;
+    });
+*/
+  }
+
+  var zoom = d3.behavior.zoom()
+    .on("zoom", draw);
+
+  svg.append("rect")
     .attr("class", "pane")
     .attr("width", width)
     .attr("height", height)
     .call(zoom);
 
-d3.csv("readme-flights.csv", function(error, data) {
-  data.forEach(function(d) {
-    d.date = parseDate(d.date);
-    d.value = parseFloat(d.value);
-  });
-
-  x.domain([new Date(1999, 0, 1), new Date(2003, 0, 0)]);
-  y.domain(d3.extent(data, function(d) { return d.value; }));
   zoom.x(x);
-  zoom.y(y);
 
-  svg.select("path.line").data([data]);
   draw();
 });
 
-function draw() {
-  svg.select("g.x.axis").call(xAxis).selectAll("text")
-                                        .style("text-anchor", "end")
-                                        .attr("dx", "-.8em")
-                                        .attr("dy", ".15em")
-                                        .attr("transform", function(d) {
-                                          return "rotate(-90)";
-
-    });
-  svg.select("g.y.axis").call(yAxis);
-  svg.select("path.line").attr("d", line);
-}
